49557f9d3b7a0937e7cf6181d76b5c28a7acc4be
yhc-key <yhcho0712@gmail.com>
2024-10-05 21:50:31 +0900

    홈 수정 중

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading]= useState(true)
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000)
+  }, [])
+
+
   return (
-    <div className="bg-[url('/assets/mainMap.png')] bg-cover bg-center w-screen h-screen">
-      <Link href="/line">
-        <button>Go to line Page</button>
-      </Link>
-      <Link href="/selecting">
-        <button>Go to selecting</button>
-      </Link>
+    <div className="bg-customBackGroundColor bg-cover bg-center w-screen h-screen flex justify-center items-center">
+      <Image src='/assets/energyGuardians.png' width="1000" height="1000"  />
+      {loading ?? <div className="absolute bottom-10 right-10 text-white">loading...</div> }
+    
     </div>
   );
 }
