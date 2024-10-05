5d7806ef8a33a1aa102f8905e5145ae57d556677
yhc-key <yhcho0712@gmail.com>
2024-10-05 18:35:33 +0900

    Map 페이지 업데이트

--- README.md
+++ README.md
@@ -1,36 +1,16 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+짬푸조 해커톤 시작합니다.
 
-## Getting Started
+총괄, 개발1 : 조용환
+개발2 : 우승빈
+개발3 : 신용화
 
-First, run the development server:
+기획1 : 박보은
+기획2 : 이지수
+기획3 : 서재연
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## 구동 방법
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+npm install 후
+npm run dev 
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,4 +1,40 @@
 
 export default function Map() {
-    return <div> map 페이지</div>
+    return (
+        <>
+          <div className="bg-[url('/assets/mainMap.png')] bg-cover bg-center w-screen h-screen">
+            <div className="absolute top-10 right-10 bg-slate-300 w-[400px] h-30 rounded-lg opacity-80">
+              <div className="m-4 mx-6 text-black text-lg font-bold ">
+                <div className="flex justify-between gap-4">
+                  <div className="h-8">
+                  Energy
+                  </div>
+                  <div className="w-[260px] h-8 bg-slate-200 rounded-xl">
+                    <div className="w-40 h-full bg-green-500 rounded-xl"/>
+                  </div>
+                    
+                </div>
+                <div className="mt-2 flex justify-between gap-4">
+                  <div className="h-8">
+                  Money
+                  </div>
+                  <div className="w-[260px] h-8 bg-slate-200 rounded-xl">
+                    <div className="w-40 h-full bg-yellow-500 rounded-xl"/>
+                  </div>
+                    
+                  </div>
+                </div>
+              </div>
+
+          </div>
+          <button className="absolute top-40 left-60 w-12 h-12 bg-[url('/assets/grayPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute bottom-40 left-[100px] w-12 h-12 bg-[url('/assets/grayPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute top-80 right-60 w-12 h-12 bg-[url('/assets/greenPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute top-[120px] right-[800px] w-12 h-12 bg-[url('/assets/greenPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute bottom-[1300px] left-[120px] w-12 h-12 bg-[url('/assets/yellowPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute top-[170px] right-[860px] w-12 h-12 bg-[url('/assets/yellowPlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute bottom-[120px] right-60 w-12 h-12 bg-[url('/assets/bluePlace.png')] bg-cover bg-center rounded-lg text-white"/>
+          <button className="absolute bottom-[120px] left-60 w-12 h-12 bg-[url('/assets/bluePlace.png')] bg-cover bg-center rounded-lg text-white"/>
+        </>
+    )
 }
