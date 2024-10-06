0302b8e097aa6a1201b655f6dadbae16159e23b3
yhc-key <yhcho0712@gmail.com>
2024-10-06 17:55:41 +0900

    추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // GitHub Pages에 배포하기 위한 설정
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://yhc-key.github.io/nasaHackathon/' : '',
+  trailingSlash: true, // 슬래시를 포함하여 URL을 정리합니다.
+};
 
 export default nextConfig;
+
